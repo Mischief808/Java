9a2dbc2c4491395a51e3d662f82b9c41b5086a8e
SAM-LCU <articulatemischief@gmail.com>
2023-03-01 07:33:19 +0530

    Mult Prog

---
+++ Samples/MultSimp.java
@@ -0,0 +1,11 @@
+class MultSimp
+{
+  public static void main(String args[])
+  {
+     float j=2.2f;
+     float k=6;
+     float result=j*k;
+     System.out.println(result);
+     System.out.println(4*6);
+   }
+}
