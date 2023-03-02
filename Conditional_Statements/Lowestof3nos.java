bf5192c99af0f9faa596522ac1a6620f4ff72054
SAM-LCU <articulatemischief@gmail.com>
2023-03-02 07:15:30 +0530

    Prog to get Lowest of 3 no's

---
+++ Conditional_Statements/Lowestof3nos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Lowestof3nos
+{
+    public static void main(String args[]) 
+    { 
+        Scanner no = new Scanner(System.in);
+        double p,q,r;
+        System.out.println("Enter Three numbers p,q,r");
+        p=no.nextDouble();
+        q=no.nextDouble();
+        r=no.nextDouble();
+        if(p<q && p<r)
+        {
+            System.out.println(p+" is Lowest number");
+        }
+        else if(q<r && q<p)
+        {
+            System.out.println(q+" is Lowest number");
+        }
+        else 
+        System.out.println(r+" is Lowest number");
+        
+    }
+}
