86f69e2ead4ccd126458c252e74d4b4ffcfc9b28
SAM-LCU <articulatemischief@gmail.com>
2023-03-02 07:13:32 +0530

    Examp of IF-ELSE

---
+++ Conditional_Statements/Ifex.java
@@ -0,0 +1,14 @@
+ class Ifex
+{
+    public static void main(String args[])
+    {
+        char a='c';
+        char b='c';
+        if(a == b)
+        {
+            System.out.println("Both are Equal");
+        }
+        else 
+        System.out.println("Both are not equal");
+    }    
+}
