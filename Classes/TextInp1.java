b1e6c2219e0bff299ae22d8883e2cd672042410d
SAM-LCU <articulatemischief@gmail.com>
2023-03-07 06:27:22 +0530

    Getting Input Using Console

--- Classes/TextInp1.java
+++ Classes/TextInp1.java
@@ -2,7 +2,12 @@ import java.io.*;
 class TextInp1
 {
     public static void main(String args[])
-    {
+    { 
+        String Color;
+        Console get = System.console();
+        System.out.println("What's Your favourite Color");
+        Color = get.readLine();
+        System.out.println("We will provide you a "+Color+" Color Shirt");
                
     }
 }
