c4b1d66980f55a76c1eefaddd9bd152f182c6a4b
SAM-LCU <articulatemischief@gmail.com>
2023-03-12 08:51:09 +0530

    Creating and accesing an arrray using For each

--- Array/Access1.java
+++ Array/Access1.java
@@ -2,15 +2,20 @@ class Access1
 {
     public static void main(String args[])
     {
-        int num[] = new int[5];
-        num[0] = 4;
-        num[1] = 6;
-        num[2] = 3;
-        num[3] = 7;
-        num[4] = 9;
-        for(int i=0;i<5;i++)
+        // int num[] = new int[5];
+        // num[0] = 4;
+        // num[1] = 6;
+        // num[2] = 3;
+        // num[3] = 7;
+        // num[4] = 9;
+        // for(int i=0;i<5;i++)
+        // {
+        //     System.out.println(num[i]);
+        // }
+        int[] var = {5,3,7,6};
+        for(int i:var)
         {
-            System.out.println(num[i]);
+             System.out.println(i);
         }
     }
 }
