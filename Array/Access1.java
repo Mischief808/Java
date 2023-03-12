6d280b9e4cca1de1ee4bf68849bd4649319f4787
SAM-LCU <articulatemischief@gmail.com>
2023-03-12 07:56:43 +0530

    Craeting and accesing an arrray using For loop

---
+++ Array/Access1.java
@@ -0,0 +1,16 @@
+class Access1
+{
+    public static void main(String args[])
+    {
+        int num[] = new int[5];
+        num[0] = 4;
+        num[1] = 6;
+        num[2] = 3;
+        num[3] = 7;
+        num[4] = 9;
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(num[i]);
+        }
+    }
+}
