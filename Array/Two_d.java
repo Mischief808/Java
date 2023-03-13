3495b423907b0b578b2b53bee0be7c721a96dcf2
SAM-LCU <articulatemischief@gmail.com>
2023-03-13 12:28:48 +0530

    TWO-D arrays EX

--- Array/Two_d.java
+++ Array/Two_d.java
@@ -2,29 +2,35 @@ class Two_d
 {
     public static void main(String args[])
     {
-        // String[][] cars={{"Hyundai","Honda","Tata"},{"BMW","Volvo","Benz","Audi"}};
+        String[][] cars={{"Hyundai","Honda","Tata"},{"BMW","Volvo","Benz","Audi"}};
+        System.out.println(cars[0][0]);
+        System.out.println(cars[0][1]);
+        
+        System.out.println(cars[0][2]);
+        
+        System.out.println(cars[1][0]);
+        
+        System.out.println(cars[1][1]);
+        
+        System.out.println(cars[1][2]);
+        
+        System.out.println(cars[1][3]);
+        
+        // int num[][] = new int[3][4];
         // for(int i=0;i<3;i++)
         // {
         //     for(int j=0;j<4;j++)
         //     {
-        //         System.out.print(cars[i][j]);
+        //         num[i][j] = (int)(Math.random()*10);
         //     }
         // }
-        int num[][] = new int[3][4];
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<4;j++)
-            {
-                num[i][j] = (int)(Math.random()*10);
-            }
-        }
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<4;j++)
-            {
-                System.out.print(num[i][j]+" ");
-            }
-            System.out.println();
-        }
+        // for(int i=0;i<3;i++)
+        // {
+        //     for(int j=0;j<4;j++)
+        //     {
+        //         System.out.print(num[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
