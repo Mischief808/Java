f4f6a1e11acbbc46806fd0c8e58e3dfa5a77328a
SAM-LCU <articulatemischief@gmail.com>
2023-03-15 06:47:18 +0530

    OK

--- Examples(Str)/Buffer.java
+++ Examples(Str)/Buffer.java
@@ -2,19 +2,19 @@ class Buffer
 {
     public static void main(String[] b)
     {
-    //    StringBuffer gh = new StringBuffer("Mathew");
-    //    gh.append(" Wade");
-    //    System.out.println(gh);
-    //    System.out.println(gh.capacity());
-    //    System.out.println(gh.insert(11," is a trainer "));
-    //    System.out.println(gh.replace(0,11,"Michael Hussey"));
-    //    System.out.println(gh.delete(18,20));
-    //    System.out.println(gh.reverse());
+       StringBuffer gh = new StringBuffer("Mathew");
+       gh.append(" Wade");
+       System.out.println(gh);
+       System.out.println(gh.capacity());
+       System.out.println(gh.insert(11," is a trainer "));
+       System.out.println(gh.replace(0,11,"Michael Hussey"));
+       System.out.println(gh.delete(18,20));
+       System.out.println(gh.reverse());
       
-       StringBuffer d  = new StringBuffer("Hi this is me dggqy echsS");
-       d.setLength(36);
-       System.out.println(d.capacity());
-       System.out.print(d.length());
+    //    StringBuffer d  = new StringBuffer("Hi this is me dggqy echsS");
+    //    d.setLength(36);
+    //    System.out.println(d.capacity());
+    //    System.out.print(d.length());
         
     }
 }
