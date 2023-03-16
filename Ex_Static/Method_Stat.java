8f0fa1d5f1a4d843354acf03b44f2ce66d07769b
SAM-LCU <articulatemischief@gmail.com>
2023-03-16 07:35:00 +0530

    Prog to create Static Methods and calling it

--- Ex_Static/Method_Stat.java
+++ Ex_Static/Method_Stat.java
@@ -1,24 +1,5 @@
 import java.util.*;
-// class MathFunctions
-// {
-//     static int add(int a, int b)
-//     {
-//         return a+b;
-//     }
-//     static int sub(int a, int b)
-//     {
-//         return a-b;
-//     }
-//     static int mul(int a, int b)
-//     {
-//         return a*b;
-//     }
-//     static int div(int a, int b)
-//     {
-//         return a/b;
-//     }
-// }
-class Method_Stat
+class MathFunctions
 {
     static int add(int a, int b)
     {
@@ -36,6 +17,25 @@ class Method_Stat
     {
         return a/b;
     }
+}
+class Method_Stat
+{
+    // static int add(int a, int b)
+    // {
+    //     return a+b;
+    // }
+    // static int sub(int a, int b)
+    // {
+    //     return a-b;
+    // }
+    // static int mul(int a, int b)
+    // {
+    //     return a*b;
+    // }
+    // static int div(int a, int b)
+    // {
+    //     return a/b;
+    // }
     public static void main(String[] d)
     {
        Scanner inp = new Scanner(System.in);
@@ -43,14 +43,14 @@ class Method_Stat
        System.out.println("Enter a and b to perform operations");
        a = inp.nextInt();
        b = inp.nextInt();
-       System.out.println("The Addition of "+a+ " and " +b+ " is " +add(a,b));
+       System.out.println("The Addition of "+a+ " and " +b+ " is " +MathFunctions.add(a,b));
        if(a>b){
-       System.out.println("The Subtraction of "+a+ " and " +b+ " is " +sub(a,b));
-       System.out.println("The Divsion of " +a+ " and " +b+ " is " +div(a,b));
+       System.out.println("The Subtraction of "+a+ " and " +b+ " is " +MathFunctions.sub(a,b));
+       System.out.println("The Divsion of " +a+ " and " +b+ " is " +MathFunctions.div(a,b));
        }
        else
        System.out.println("As " +a+ " is lesser than " +b+ " Subtraction and Division are not performed");
-       System.out.println("The Multiplication of "+a+ " and " +b+ " is " +mul(a,b));
+       System.out.println("The Multiplication of "+a+ " and " +b+ " is " +MathFunctions.mul(a,b));
         
     }
 }
