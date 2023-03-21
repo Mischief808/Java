133d2b22ca565422a9e47ea288e6e3cb09c7f3c4
SAM-LCU <articulatemischief@gmail.com>
2023-03-21 11:10:50 +0530

    Small ex prog for inheritance

---
+++ Inheritance/Show.java
@@ -0,0 +1,10 @@
+class Show extends Channel
+{
+    public static void main(String[] d)
+    {
+        Television t = new Television();
+        Channel c = new Channel();
+        t.show();
+        c.show();
+    }
+}
