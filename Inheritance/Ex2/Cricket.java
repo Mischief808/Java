8195186cccf3d0d99cd367bab6c008dd28307307
SAM-LCU <articulatemischief@gmail.com>
2023-03-24 22:05:18 +0530

    Inheritance Program

---
+++ Inheritance/Ex2/Cricket.java
@@ -0,0 +1,22 @@
+class Cricket
+{
+    private String players;
+    private int no;
+    private String umpire;
+    Cricket(String players,int no,String umpire)
+    {
+        this.players = players;
+        this.no = no;
+        this.umpire = umpire;
+    }
+    void disp()
+    {      
+        System.out.println(players+" is a Player");
+        System.out.println(no+" players must be availavle in the team");
+        System.out.println(umpire+" is umpire ");
+    }
+    void hlo()
+    {
+        System.out.println("Hello you are here");
+    }
+}
