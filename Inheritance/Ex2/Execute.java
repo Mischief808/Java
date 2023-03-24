9465e834504da225b7151b63e4ece1df0b898772
SAM-LCU <articulatemischief@gmail.com>
2023-03-24 22:04:27 +0530

    Inheritance Program

---
+++ Inheritance/Ex2/Execute.java
@@ -0,0 +1,15 @@
+class Execute extends ChennaiSuperKings
+{
+    Execute(int no, String players, String umpire, int num, String coach)
+     {
+        super(no, players, umpire, num, coach);
+    }
+
+    public static void main(String args[])
+    {
+       ChennaiSuperKings ob = new ChennaiSuperKings(11,"MS Dhoni","Vineeth Srinivaan",15,"Robin Peterson");
+       ob.show();
+       ob.disp();
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                       
