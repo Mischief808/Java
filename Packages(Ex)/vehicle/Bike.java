2b15ca20c1be495b3f0e7ed60c3309a1b3d83e57
SAM-LCU <articulatemischief@gmail.com>
2023-03-28 10:00:13 +0530

    Using Diff Access modifiers by creating user defined packages

---
+++ Packages(Ex)/vehicle/Bike.java
@@ -0,0 +1,15 @@
+package vehicle;
+
+ public class Bike 
+{
+    int wheels(int tyres)
+    {
+        tyres = 2;
+        return tyres;
+    } 
+    protected void display()
+    {
+        System.out.println("This is Bike class");
+    }
+    
+}
