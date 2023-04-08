1e5cb558ae7ef8bcfeac149e2d7d00d756d26275
SAM-LCU <articulatemischief@gmail.com>
2023-04-08 21:41:49 +0530

    Prog for method overlaoding

--- Samples/Overload.java
+++ Samples/Overload.java
@@ -3,20 +3,20 @@ class Fun
 {
     int add(int a1,int a2)
     {
-      Scanner inp = new Scanner(System.in); 
-      System.out.println("Enter Numbers to add");
-      a1 = inp.nextInt();
-      a2 = inp.nextInt();
+    //   Scanner inp = new Scanner(System.in); 
+    //   System.out.println("Enter Numbers to add");
+    //   a1 = inp.nextInt();
+    //   a2 = inp.nextInt();
       return a1+a2; 
     }
-    int add1(int b1,int b2,int b3)
+    int add(int a1,int a2,int a3)
     {
-        Scanner inp = new Scanner(System.in);
-        System.out.println("Enter Numbers to add");
-        b1 = inp.nextInt();
-        b2 = inp.nextInt();
-        b3 = inp.nextInt();
-        return b1+b2+b3;
+        // Scanner inp = new Scanner(System.in);
+        // System.out.println("Enter Numbers to add");
+        // b1 = inp.nextInt();
+        // b2 = inp.nextInt();
+        // b3 = inp.nextInt();
+        return a1+a2+a3;
     }
 }
 class Overload 
