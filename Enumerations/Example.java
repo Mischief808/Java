6f4be6c476248137c74b467d16ebaf9c9cf5f4b1
SAM-LCU <articulatemischief@gmail.com>
2023-04-18 10:43:42 +0530

    Small Ex prog for Enumerations

--- Enumerations/Example.java
+++ Enumerations/Example.java
@@ -2,32 +2,38 @@ class Example
 {
     enum Month
     {
-        January,Febuary,March,April,May,June,July,August,September,October,November,December;
-        // January(1),Febuary(2),March(3),April(4),May(5),June(6),July(7),August(8),September(9),October(10),November(11),December(12);
-        // private int num;
-        // private Month(int num)
-        // {
-        //     this.num = num;
-        // }
-        // void setNum(int num)
-        // {
-        //     this.num = num;
-        // }
-        // int getNum()
-        // {
-        //    return num;
-        // }
-
+        // January,Febuary,March,April,May,June,July,August,September,October,November,December;
+        January(1),Febuary(2),March(3),April(4),May(5),June(6),July(7),August(8),September(9),October(10),November(11),December(12);
+        private int num;
+        private Month(int num)
+        {
+            this.num = num;
+        }
+        void setNum(int num)
+        {
+            this.num = num;
+        }
+        int getNum()
+        {
+           return num;
+        }
     }
     public static void main(String d[])
     {
-        Month[] mg = Month.values();
+        // Month[] mg = Month.values();
         // Month mg1 = Month.January;
         // System.out.println(mg1+ ":" +mg1.getNum());
-        for(Month m : mg)
+
+        // Using For-Each
+         
+        for(Month mg:Month.values())
         {
-            System.out.println(m +":" +m.ordinal());
+            System.out.println(mg + " : " +mg.getNum());
         }
+        // for(Month m : mg)
+        // {
+        //     System.out.println(m +":" +m.ordinal());
+        // }
         
     }
 }
