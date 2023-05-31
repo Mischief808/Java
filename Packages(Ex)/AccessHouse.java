15ad2b1d0f141b634bf64cbb63fede716ce8cdd9
SAM-LCU <articulatemischief@gmail.com>
2023-05-31 16:49:19 +0530

    prog for accessing user-defined package

---
+++ Packages(Ex)/AccessHouse.java
@@ -0,0 +1,13 @@
+import house.*;
+class AccessHouse
+{
+ public static void main(String args[])
+ {
+    Hall obj1 = new Hall();
+    Door obj2 = new Door();
+    Kitchen obj3 = new Kitchen();
+    obj1.disp();
+    obj2.show();
+    obj3.displ();
+ }   
+}
