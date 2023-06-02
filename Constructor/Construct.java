3ac71a07936da8bc2335af3308ab8257cf61b062
SAM-LCU <articulatemischief@gmail.com>
2023-06-02 16:33:48 +0530

    Small Rectification in Example prog for Constructor

--- Constructor/Construct.java
+++ Constructor/Construct.java
@@ -2,30 +2,31 @@ class A
 {
  A()
  {
-    System.out.println("In A con");
+    System.out.println("In A constructor");
  }
  A(int n)
  {  
-    System.out.println("In A int con");
+    System.out.println("In A int constructor");
  }
 }
 class B extends A
 {
  B()
  {
-    System.out.println("In B con ");
+    System.out.println("In B constrcutor");
  }
  B(int n)
  { 
-    System.out.println("In B int con");
+   super(n); //calling parent class Constructor
+    System.out.println("In B int constructor");
  }
 }
 class Construct
 {
     public static void main(String args[])
     {
-         
-      B ob1 = new B(6); 
+      A ob = new B();
+      B ob1 = new B(5); 
 
     }
 }
