42dcf2eeb0a1bd88f1c87761fb93775641b93168
SAM-LCU <articulatemischief@gmail.com>
2023-06-05 12:13:42 +0530

    (File)

--- Interfaces/Materials.java
+++ Interfaces/Materials.java
@@ -6,32 +6,32 @@ class Bricks implements Material
 {
     public void info()
     {
-        System.out.println("This is Bricks");
+        System.out.println("This is Bricks class");
     }
 }
 class Sand implements Material
 {
     public void info()
     {
-        System.out.println("This is Sand");
+        System.out.println("This is Sand class");
     }
 }
 class Acces
 {
-    public void getMat(Material bri)
+    public void getMater(Material bri)
     {
         bri.info();
     }
 }
 class Materials
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     { 
         Material bri = new Bricks();
         Material San = new Sand();
 
         Acces dud = new Acces();
-        dud.getMat(bri);
-        dud.getMat(San);
+        dud.getMater(bri);
+        dud.getMater(San);
     }
 }
