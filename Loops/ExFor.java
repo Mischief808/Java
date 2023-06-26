dd206840b13078fe5706bc83f98e53aeb7eb0097
SAM-LCU <articulatemischief@gmail.com>
2023-06-26 21:51:23 +0530

    For LOOP Ex

--- Loops/ExFor.java
+++ Loops/ExFor.java
@@ -3,10 +3,10 @@ class ExFor
 {
    public static void main(String args[])
    {
-     Scanner inp = new Scanner(System.in);
+     Scanner inpt = new Scanner(System.in);
      int i,k;
-     System.out.println("Enter number get mutiples of it");
-     k = inp.nextInt();
+     System.out.println("Enter the number get mutiples of it");
+     k = inpt.nextInt();
      System.out.println("Multiples of "+k+" are");
      for(i=0;i<=100;i=i+k)
      { 
