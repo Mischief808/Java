a4348ce82309d5d8b05b6b5c459b9025b9f5c637
SAM-LCU <articulatemischief@gmail.com>
2023-07-02 08:43:43 +0530

    (File)

--- Dynamic_Dispatch/NonVegeterianDispatch.java
+++ Dynamic_Dispatch/NonVegeterianDispatch.java
@@ -24,21 +24,21 @@ class NonVegeterianDispatch
 {
     public static void main(String[] h)
     {
-      //  NonVegeterian obj = new NonVegeterian();
-      //  Chicken obj1 = new Chicken();
-      //  Prawn obj2 = new Prawn(); 
-      //  NonVegeterian acc;
-      //  acc = obj;
-      //  acc.display();
-      //  acc = obj1;
-      //  acc.display();
-      //  acc = obj2;
-      //  acc.display();
+       NonVegeterian obj = new NonVegeterian();
+       Chicken obj1 = new Chicken();
+       Prawn obj2 = new Prawn(); 
+       NonVegeterian acc;
+       acc = obj;
+       acc.display();
+       acc = obj1;
+       acc.display();
+       acc = obj2;
+       acc.display();
 
-      NonVegeterian obj = new Chicken();
-      obj.display();
+      // NonVegeterian obj = new Chicken();
+      // obj.display();
        
-      NonVegeterian obj1 = (Chicken) obj;
-      obj1.display();
+      // NonVegeterian obj1 = (Chicken) obj;
+      // obj1.display();
     }
 }
