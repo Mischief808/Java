4f6e8bc3521ba8d719e487b348b4bca5fc8d991c
SAM-LCU <articulatemischief@gmail.com>
2023-07-26 18:24:26 +0530

    Example of Dynamic Method Dispatch

--- Dynamic_Dispatch/NonVegeterianDispatch.java
+++ Dynamic_Dispatch/NonVegeterianDispatch.java
@@ -2,21 +2,21 @@ class NonVegeterian
 {
   public void display()
   {
-    System.out.println("Show Method in Non-Veg");
+    System.out.println("Non-Veg's show method");
   }
 }
 class Chicken extends NonVegeterian
 {
   public void display()
   {
-    System.out.println("Show Method in Chicken");
+    System.out.println("Chicken's show method");
   }
 }
 class Prawn extends NonVegeterian
 {
   public void display()
   {
-    System.out.println("Show Method in Prawn");
+    System.out.println("Prawn's show method");
   }
 }
 
@@ -24,21 +24,21 @@ class NonVegeterianDispatch
 {
     public static void main(String[] h)
     {
-      //  NonVegeterian obj = new NonVegeterian();
-      //  Chicken obj1 = new Chicken();
-      //  Prawn obj2 = new Prawn(); 
-      //  NonVegeterian acc;
-      //  acc = obj;
-      //  acc.display();
-      //  acc = obj1;
-      //  acc.display();
-      //  acc = obj2;
-      //  acc.display();
+       NonVegeterian obj = new NonVegeterian();
+       Chicken obj1 = new Chicken();
+       Prawn obj2 = new Prawn(); 
+       NonVegeterian acc;
+       acc = obj;
+       acc.display();
+       acc = obj1;
+       acc.display();
+       acc = obj2;
+       acc.display();
 
-      NonVegeterian obj = new Chicken();
-      obj.display();
+      // NonVegeterian obj = new Chicken();
+      // obj.display();
        
-      NonVegeterian obj1 = (Chicken) obj;
-      obj1.display();
+      // NonVegeterian obj1 = (Chicken) obj;
+      // obj1.display();
     }
 }
